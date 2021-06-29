0112c7a397cf1821e3ecfc2e1ab429a5f8d58ae1
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-29 20:51:57 -0300

    :package: Adicionando rota de autenticação de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
+const authenticateUserController = new AuthenticateUserController();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 
+router.post("/auth", authenticateUserController.handle);
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
 
