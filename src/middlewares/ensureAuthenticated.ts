3b53fdc6c1e43621a1bb07691d0acf4834877fe6
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-30 19:06:35 -0300

    :package: Criando middleware de verificar token

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+  sub: string;
+}
+
+export function ensureAdmin(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authToken = request?.headers?.authorization;
+
+  if(!authToken) {
+    return response.status(401).end();
+  }
+
+  const [, token] = authToken.split(" ");
+
+  try {
+    const { sub } = verify(token, "c012754f5acbd1e47db7d50864d98af1") as IPayload;
+
+    request.user_id = sub;
+
+    next();
+  } catch (error) {
+    return response.status(401).end();
+  }
+
+  return next();
+}
