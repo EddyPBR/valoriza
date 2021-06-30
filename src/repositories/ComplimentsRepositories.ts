5a1c086f34944fe5db3be5f19d43212a2404174a
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-30 09:36:17 -0300

    :package: Criando compliments repository

---
+++ src/repositories/ComplimentsRepositories.ts
@@ -0,0 +1,7 @@
+import { Repository, EntityRepository } from "typeorm";
+import { Compliment } from "../entities/Compliment";
+
+@EntityRepository(Compliment)
+class ComplimentRepositories extends Repository<Compliment> {}
+
+export { ComplimentRepositories };
