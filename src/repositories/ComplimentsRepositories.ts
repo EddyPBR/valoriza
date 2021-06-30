8036a1438b2d1760d73c81ba08e4af699eb456e2
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-30 09:47:51 -0300

    :wrench: Renomenado class compliment repositories

--- src/repositories/ComplimentsRepositories.ts
+++ src/repositories/ComplimentsRepositories.ts
@@ -2,6 +2,6 @@ import { Repository, EntityRepository } from "typeorm";
 import { Compliment } from "../entities/Compliment";
 
 @EntityRepository(Compliment)
-class ComplimentRepositories extends Repository<Compliment> {}
+class ComplimentsRepositories extends Repository<Compliment> {}
 
-export { ComplimentRepositories };
+export { ComplimentsRepositories };
