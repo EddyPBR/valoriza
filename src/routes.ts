4e1776abc7cec136103ac955e43e55a316ba4b1b
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-30 19:26:09 -0300

    :package: Alterando/adicionando middlewares

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
 const router = Router();
 
@@ -13,8 +14,20 @@ const createTagController = new CreateTagController();
 const createComplimentController = new CreateComplimentController();
 
 router.post("/auth", authenticateUserController.handle);
+
 router.post("/users", createUserController.handle);
-router.post("/tags", ensureAdmin, createTagController.handle);
-router.post("/compliments", createComplimentController.handle);
+
+router.post(
+  "/tags",
+  ensureAuthenticated,
+  ensureAdmin,
+  createTagController.handle
+);
+
+router.post(
+  "/compliments",
+  ensureAuthenticated,
+  createComplimentController.handle
+);
 
 export { router };
