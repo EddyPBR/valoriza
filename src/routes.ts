f9123028a40a529c5badef12c4f71a64c58239f8
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-30 10:58:20 -0300

    :package: Criando rota de cadastrar elogios

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
@@ -9,9 +10,11 @@ const router = Router();
 const authenticateUserController = new AuthenticateUserController();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/auth", authenticateUserController.handle);
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/compliments", createComplimentController.handle);
 
 export { router };
