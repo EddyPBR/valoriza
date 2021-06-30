0ca14abd1cdcaefb1299054be8e6e97ae153777e
EddyPBR <edvaldojunior1310@gmail.com>
2021-06-30 20:34:06 -0300

    :package: Criando rota de listar usuários

--- src/routes.ts
+++ src/routes.ts
@@ -3,26 +3,29 @@ import { AuthenticateUserController } from "./controllers/AuthenticateUserContro
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
+import { ListUsersController } from "./controllers/ListUsersController";
 import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
 import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
 import { ListTagsController } from "./controllers/ListTagsController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
-const router = Router();
-
 const authenticateUserController = new AuthenticateUserController();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const createComplimentController = new CreateComplimentController();
+const listUsersController = new ListUsersController();
 const listUserReceiveComplimentsController =
   new ListUserReceiveComplimentsController();
 const listUserSendeComplimentsController =
   new ListUserSendComplimentsController();
 const listTagsController = new ListTagsController();
 
+const router = Router();
+
 router.post("/auth", authenticateUserController.handle);
 
+router.get("/users", listUsersController.handle);
 router.post("/users", createUserController.handle);
 
 router.get("/tags", listTagsController.handle);
